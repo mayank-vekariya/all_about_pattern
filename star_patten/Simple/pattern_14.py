388f34e1fc2110fba74650c26cdde1a1ac68afc6
Maynk Vekariya <mayankpv2000@gmail.com>
2021-08-06 18:25:58 +0530

    more_pattern_add

--- star_patten/Simple/pattern_14.py
+++ star_patten/Simple/pattern_14.py
@@ -1,7 +1,14 @@
 def Pattern_Programs(n):
-	for i in range(1,n+1):
-		print("*"*i)
-
+	star=n
+	osp=0
+	for i in range(1,n*2):
+		print(" "*osp+"* "*star)
+		if i<n:
+			star-=1
+			osp+=1
+		else:
+			star+=1
+			osp-=1
 
 if __name__ == '__main__':
 	Pattern_Programs(int(input()))
