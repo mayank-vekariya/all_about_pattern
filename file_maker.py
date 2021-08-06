32b9033828373f00d5dac663bde101faa77c4939
Maynk Vekariya <mayankpv2000@gmail.com>
2021-08-06 18:04:07 +0530

    more_pattern_add

--- file_maker.py
+++ file_maker.py
@@ -1,4 +1,4 @@
-# for i in range(5,9):
-#     with open(f'star_patten/Simple/pattern_{i}.py', 'w') as f:
-#         data = 'def Pattern_Programs(n):\n\tfor i in range(1,n+1):\n\t\tprint("*"*i)\n\n\nif __name__ == \'__main__\':\n\tPattern_Programs(int(input()))'
-#         f.write(data)
+for i in range(9,16):
+    with open(f'star_patten/Simple/pattern_{i}.py', 'w') as f:
+        data = 'def Pattern_Programs(n):\n\tfor i in range(1,n+1):\n\t\tprint("*"*i)\n\n\nif __name__ == \'__main__\':\n\tPattern_Programs(int(input()))'
+        f.write(data)
